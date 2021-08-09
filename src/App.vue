95ba10c20bd6aa14c4464a0c6de6a02ea5d1c771
vslotema <vslotema@gmail.com>
2021-08-09 14:43:27 +0200

    autocomplete search

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <div class="header">
+        <h1 class="text-center">MovieDB</h1>
+        <v-autocomplete
+          :items="items"
+          :search-input.sync="search"
+          :loading="loading"
+          v-model="selected"
+          placeholder="Search in MovieDB"
+          clearable
+          dense
+          solo
+          cache-items
+          item-text="Title"
+          item-value="Title"
+           hide-no-data
+          return-object
+        ></v-autocomplete>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    items: [],
+    key: "20b15791",
+    selected: "",
+    search: "",
+    loading: false,
+  }),
+
+  watch: {
+    selected(val) {
+      console.log("selected ", val);
+    },
+    search(val) {
+      console.log("Search ", val);
+      if (val && val.length >= 3) this.fetchData();
+    },
+  },
+
+  methods: {
+    fetchData() {
+      this.loading = true;
+      axios
+        .get(`http://www.omdbapi.com/?apikey=${this.key}&`, {
+          params: {
+            s: this.search,
+            type: "movie",
+            page: 1,
+          },
+        })
+        .then((response) => {
+          console.log("RESPONSE ", response);
+          this.items = response.data.Search;
+          console.log("items ", this.items);
+        })
+        .catch((error) => console.log("error", error))
+        .finally(() => (this.loading = false));
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h1 {
+  color: white;
+  padding: 1rem 0rem 0.5rem 0rem;
+}
+.header {
+  background: rgb(22, 22, 22);
+  padding: 0rem 1rem 0rem 1rem;
 }
 </style>
